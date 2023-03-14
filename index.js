d69ce80d1bb2b169102851f96955f67d89414cd7
Nyaromollo <omolloenter@gmail.com>
2023-03-14 20:25:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let max = Math.max(10,20);
+max;
+
+const num3 = 29;
+const num4 =5;
+let mod = 29%5;
+
+const num1 = 31;
+const num2 = 2;
+let multiply = 31*2;
+
+const number = 1;
+const random = 1;
